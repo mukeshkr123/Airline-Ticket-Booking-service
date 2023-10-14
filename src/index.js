02f9e50be4b5201474838815b23c5affc88e21d0
Mukesh Kumar <mkmehta2041@gmail.com>
2023-10-14 19:49:26 +0530

    basic setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
-const PORT = 3002;
+const { PORT } = require("./config/serverConfig");
+const apiRoutes = require("./routes/index");
+const db = require("./models/index");
 
 const setupAndStartServer = () => {
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/api", apiRoutes);
+
   app.listen(PORT, () => {
     console.log(`Server is listening at ${PORT}`);
+    if (process.env.DB_SYNC) {
+      db.sequelize.sync({ alter: true });
+    }
   });
 };
 
